c50d4590e14c6ee0e643838bec62151df5b243de
CichockiAdrian <adrian.cichocki@uczen.zsk.poznan.pl>
2024-10-29 23:18:03 +0100

    add database

--- express_v2/index.js
+++ express_v2/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const port = 3000;
+const mysql = require('mysql');
 
 const app = express();
 
@@ -28,7 +29,50 @@ app.get('/kontakt', (req, res) => {
 app.post('/kontakt', (req, res) => {
     const { name, surname, email, message } = req.body;
     console.log(`[${name} ${surname}, ${email}, ${message}]`);
-    res.redirect('/');
+    //nowa część
+    const sql = 'INSERT INTO messages (name, surname, email, message) VALUES (?, ?, ?, ?)';
+    db.query(sql, [name, surname, email, message], (err, result) => {
+        if (err) {
+            console.error(err);
+            return res.status(500);
+        }
+
+        res.redirect('/');
+    });
+});
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'messages'
+});
+
+db.connect(err => {
+    if (err) throw err;
+});
+
+app.get('/api/contact-messages', (req, res) => {
+    const sql = 'SELECT * FROM messages';
+    db.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500);
+        }
+        res.json(results);
+    });
+});
+
+app.get('/api/contact-messages/:id', (req, res) => {
+    const sql = 'SELECT * FROM messages WHERE id = ?';
+    db.query(sql, [req.params.id], (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: 'Błąd podczas pobierania wiadomości' });
+        }
+        if (result.length === 0) {
+            return res.status(404).json({ error: 'Wiadomość nie znaleziona' });
+        }
+        res.json(result[0]);
+    });
 });
 
 app.listen(port, () => {
